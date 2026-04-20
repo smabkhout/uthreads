2566b68779f2d5e30dcaf1e54a50c6eb368c3223
Anas Benbaki <abenbaki@enseirb-matmeca.fr>
2026-04-20 16:38:07 +0200

    fix last commit

--- test/example.c
+++ test/example.c
@@ -1,22 +1,20 @@
 #include "../src/thread.h"
-#include <stdio.h>
 #include <assert.h>
+#include <stdio.h>
 
-static void * threadfunc(void * arg)
-{
+static void *threadfunc(void *arg) {
   char *name = arg;
   printf("je suis le thread %p, lancé avec l'argument %s\n",
-	 (void*) thread_self(), name);
+         (void *)thread_self(), name);
   thread_yield();
   printf("je suis encore le thread %p, lancé avec l'argument %s\n",
-	 (void*) thread_self(), name);
+         (void *)thread_self(), name);
   thread_exit(arg);
 }
 
-int main(int argc, char *argv[])
-{
-  (void) argc;
-  (void) argv;
+int main(int argc, char *argv[]) {
+  (void)argc;
+  (void)argv;
   thread_t thread1, thread2;
   void *retval1, *retval2;
   int err;
@@ -26,8 +24,8 @@ int main(int argc, char *argv[])
   assert(!err);
   err = thread_create(&thread2, threadfunc, "thread2");
   assert(!err);
-  printf("le main a lancé les threads %p et %p\n",
-	 (void*) thread1, (void*) thread2);
+  printf("le main a lancé les threads %p et %p\n", (void *)thread1,
+         (void *)thread2);
 
   printf("le main attend les threads\n");
   err = thread_join(thread2, &retval2);
@@ -35,7 +33,7 @@ int main(int argc, char *argv[])
   err = thread_join(thread1, &retval1);
   assert(!err);
   printf("les threads ont terminé en renvoyant '%s' and '%s'\n",
-	 (char *) retval1, (char *) retval2);
+         (char *)retval1, (char *)retval2);
 
   return 0;
 }
