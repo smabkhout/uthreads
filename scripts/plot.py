79d013bf7a9056e70986bd0b4574558acb342ea2
MIGAOU Mohamed_Aziz <Mohamed_Aziz.Migaou@bordeaux-inp.fr>
2026-04-27 16:51:37 +0100

    graphs avec diff implementation

--- scripts/plot.py
+++ scripts/plot.py
@@ -86,15 +86,34 @@ def main():
         setjmp_bin = os.path.join(BIN_DIR, test)
         context_bin = os.path.join(BIN_DIR, test + "-context")
         pthread_bin = os.path.join(BIN_DIR, test + "-pthread")
+        one_malloc_bin = os.path.join(BIN_DIR, test + "-one-malloc")
+        recycle_bin = os.path.join(BIN_DIR, test + "-recycle")
+        one_malloc_recycle_bin = os.path.join(BIN_DIR, test + "-one-malloc-recycle")
+        preem_bin = os.path.join(BIN_DIR, test + "-preem")
+        stackprot_bin = os.path.join(BIN_DIR, test + "-stackprot")
 
 
-        if not os.path.exists(setjmp_bin) or not os.path.exists(pthread_bin) or not os.path.exists(context_bin):
+        if (
+            not os.path.exists(setjmp_bin)
+            or not os.path.exists(context_bin)
+            or not os.path.exists(pthread_bin)
+            or not os.path.exists(one_malloc_bin)
+            or not os.path.exists(recycle_bin)
+            or not os.path.exists(one_malloc_recycle_bin)
+            or not os.path.exists(preem_bin)
+            or not os.path.exists(stackprot_bin)
+        ):
             print(f"[SKIP] {test} non trouvé")
             continue
 
         setjmp_times = []
         context_times = []
         pthread_times = []
+        one_malloc_times = []
+        recycle_times = []
+        one_malloc_recycle_times = []
+        preem_times = []
+        stackprot_times = []
         
 
         for n in THREADS:
@@ -102,16 +121,31 @@ def main():
 
             t1 = average_time(setjmp_bin, n)
             t3 = average_time(context_bin, n)
-            t2 = average_time(pthread_bin, n)   
+            t2 = average_time(pthread_bin, n)
+            t4 = average_time(one_malloc_bin, n)
+            t5 = average_time(recycle_bin, n)
+            t6 = average_time(one_malloc_recycle_bin, n)
+            t7 = average_time(preem_bin, n)
+            t8 = average_time(stackprot_bin, n)
             
     
             print(f"  setjmp/longjmp avg: {t1}")
             print(f"  ucontext avg: {t3}")
             print(f"  Pthreads avg: {t2}")
+            print(f"  one-malloc avg: {t4}")
+            print(f"  recycle avg: {t5}")
+            print(f"  one-malloc-recycle avg: {t6}")
+            print(f"  preem avg: {t7}")
+            print(f"  stackprot avg: {t8}")
 
             setjmp_times.append(t1)
             context_times.append(t3)
             pthread_times.append(t2)
+            one_malloc_times.append(t4)
+            recycle_times.append(t5)
+            one_malloc_recycle_times.append(t6)
+            preem_times.append(t7)
+            stackprot_times.append(t8)
             
 
         # graphe
@@ -120,6 +154,11 @@ def main():
         plt.plot(THREADS, setjmp_times, marker='o', label="setjmp/longjmp")
         plt.plot(THREADS, context_times, marker='o', label="ucontext")
         plt.plot(THREADS, pthread_times, marker='o', label="Pthreads")
+        plt.plot(THREADS, one_malloc_times, marker='o', label="one-malloc")
+        plt.plot(THREADS, recycle_times, marker='o', label="recycle")
+        plt.plot(THREADS, one_malloc_recycle_times, marker='o', label="one-malloc-recycle")
+        plt.plot(THREADS, preem_times, marker='o', label="preem")
+        plt.plot(THREADS, stackprot_times, marker='o', label="stackprot")
 
         plt.xlabel("Number of threads")
         plt.ylabel("Execution time (s)")
